b552732bf3f6bcd2613cee5dcd965a5978b297e2
Rabin <rabing624@gmail.com>
2025-04-09 22:21:53 +0545

    user form

--- Frontend/src/assets/pages/Home.jsx
+++ Frontend/src/assets/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import image1 from '../images/image1.jpeg'
+
 
 const Home = () => {
   return (
     <div> 
-        <div className='h-screen w-full flex justify-between flex-col bg-[#33a852]' >
-           <img src="" alt="" />
-            <div className='bg-white'>
-                <h2>Lets start the Ride</h2>
-                <button>Start</button>
+        <div   style={{ backgroundImage: `url(${image1})` }}  className='bg-cover bg-center pt-5 h-screen w-full flex justify-between flex-col bg-[#33a852]' >
+           <img className=' w-20 ml-8' src="https://imgs.search.brave.com/yfhjvPmbpij5DnmuCDlZMi4F2HgTGPqGRKLV3SCp3LM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJ1cGxvYWQv/NDExMDE0MjcvZmls/ZS9vcmlnaW5hbC0y/YTY2NmQxMzUzOWRh/MzdkNzg1ZmVmODAy/ZWIyOTk2Ni5qcGc_/cmVzaXplPTQwMHgw" alt="" />
+            <div className='bg-white pb-5 py-5 px-5'>
+                <h2 className='text-3xl font-bold'>Lets start the Ride</h2>
+                <Link to='/login' className=' flex items-center justify-center w-full bg-black text-white mt-4 py-3 rounded-lg' >Start</Link>
             </div>
         </div>
     </div>
--- Frontend/src/assets/pages/UserLogin.jsx
+++ Frontend/src/assets/pages/UserLogin.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+   <div className='p-7'>
+
+  <img className=' w-20 ' src="https://imgs.search.brave.com/yfhjvPmbpij5DnmuCDlZMi4F2HgTGPqGRKLV3SCp3LM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/ZHJpYmJibGUuY29t/L3VzZXJ1cGxvYWQv/NDExMDE0MjcvZmls/ZS9vcmlnaW5hbC0y/YTY2NmQxMzUzOWRh/MzdkNzg1ZmVmODAy/ZWIyOTk2Ni5qcGc_/cmVzaXplPTQwMHgw" alt="" />
+    <form action="" className='mt-10'>
+      <h3 className='text-xl font-medium mb-2'>Enter the email</h3>
+      <input  className='bg-[#c5c2c2] text-lg mb-5  rounded px-4 py-2 border w-full'
+      required 
+      type="email"
+       placeholder='something@example.com'/>
+      <h3 className='text-xl font-medium mb-2'>Enter password</h3>
+      <input className='bg-[#c5c2c2] mb-5 text-lg  rounded px-4 py-2 border w-full'
+       required 
+       type="password" 
+       placeholder='********'/>
+      <button className = ' w-full bg-black text-white mt-4 py-3 rounded-lg'
+      >Log In</button>
+    </form>
+   </div>
   )
 }
 
